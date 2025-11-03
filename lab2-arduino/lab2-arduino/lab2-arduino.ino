b39e06dc8d3347fa7c2852074c6ba9d0d7c73906
Lucas Di Pietro <59773132+lucs100@users.noreply.github.com>
2025-11-03 18:34:21 -0500

    fix minor catie buges

--- lab2-arduino/lab2-arduino/lab2-arduino.ino
+++ lab2-arduino/lab2-arduino/lab2-arduino.ino
@@ -1,9 +1,10 @@
 #include <Arduino.h>
 #include "geeWhiz.h"
 #include <cppQueue.h>
-#define	IMPLEMENTATION	FIFO
 
-
+#define	IMPLEMENTATION FIFO
+#define n_zeroes 26
+#define n_poles 27
 
 //INSTRUCTIONS:
 //TO ADJUST KP, UNCOMMENT THE RIGHT LINE AND RECOMPILE AND RE UPLOAD.
@@ -14,15 +15,71 @@
 float rad_mag[] = {M_PI/8, -M_PI/8, M_PI/4, -M_PI/4};
 int num_steps = sizeof(rad_mag) / sizeof(rad_mag[0]);
 int step_index = 0;
-bool autostep = true;
+bool autostep = false;
 
 //================ Queue Implementation
 
 cppQueue u_queue(sizeof(double), n_poles, IMPLEMENTATION);
 cppQueue e_queue(sizeof(double), n_zeroes, IMPLEMENTATION);
 
-double a_coeffs[n_zeroes] = [cofficients go here]; // coefficients on the numerator of D
-double b_coeffs[n_poles] = [coefficients go here]; //coefficients on the denominator of D
+double a_coeffs[n_zeroes] = {
+  -4.014671308410470,
+  6.595993799639460,
+  -3.837648018627513,
+  0.708976866805470,
+  -0.025995388538642,
+  -0.076514941608012,
+  -0.032012691794383,
+  -0.037463474376800,
+  -0.035306296626180,
+  -0.048345742227537,
+  -0.038158308502171,
+  -0.058766459874832,
+  -0.013873291178805,
+  -0.043106553671307,
+  -0.044733692793647,
+  -0.039419151035545,
+  -0.038595190582891,
+  -0.033039877980923,
+  0.028878298445661,
+  -0.197134924415200,
+  0.053894813684000,
+  0.215191055728711,
+  -0.810608196173737,
+  0.833830782048386,
+  -0.088481927716491,
+  0.004242677516370,
+}; // coefficients on the numerator of D
+
+double b_coeffs[n_poles] = {
+  1.000000000000000,
+  -1.642984519812282,
+  0.958321968432410,
+  -0.183217773360566,
+  0.006068215325551,
+  0.016793826989395,
+  0.005376007191384,
+  0.006431007500784,
+  0.005490457115507,
+  0.008359531301715,
+  0.005382362473137,
+  0.010070061195247,
+  -0.000666309620383,
+  0.005730533574389,
+  0.005043448210347,
+  0.004041500950617,
+  0.003693869678733,
+  0.003322130108701,
+  0.002947770343532,
+  0.002569995478323,
+  0.002199729812247,
+  0.002003577307391,
+  0.001471797034653,
+  0.000970219358741,
+  0.001403748774129,
+  -0.000165534783335,
+  0.000008696897701,
+}; //coefficients on the denominator of D
 
 
 
@@ -76,12 +133,12 @@ double map_potentiometer(int val) {
   return radians;
 }
 
-double (double ref, double error){
-  //where ref is the reference angle in radians
-  //and y is the actual angle in radian
+// double (double ref, double error){
+//   //where ref is the reference angle in radians
+//   //and y is the actual angle in radian
 
 
-}
+// }
 
 // ================== Setup ==================
 void setup() {
@@ -221,6 +278,7 @@ void interval_control_code(void) {
     // --- Generate the reference signal based on the selected mode ---
     if (input_mode == 1) {
       original_ref = target_angle;
+      final_ref = original_ref;
     } 
     else { // input_mode == 2, calculate sin wave
       float current_time_s = (float)(T * i) / 1000.0;
@@ -237,16 +295,16 @@ void interval_control_code(void) {
     // ---- Proportional Control Law ----
     
     double error = final_ref - current_angle;
-    e_queue.push(error);
+    e_queue.push(&error);
     double u_total = 0.0;
     double peeker;
 
-    for(j = 1, j<n_poles, j++){
+    for(int j = 1; j<n_poles; j++){
       u_queue.peekIdx(&peeker, j);
       u_total -= b_coeffs[j]*peeker;
     }
 
-    for(j = 0, j<n_zeroes, j++){
+    for(int j = 0; j<n_zeroes; j++){
       e_queue.peekIdx(&peeker, j);
       u_total -= a_coeffs[j]*peeker;
     }
@@ -255,7 +313,7 @@ void interval_control_code(void) {
     if (abs(U) > 0.01) {
       U = U + (U > 0 ? stiction_offset_pos : stiction_offset_neg);
     }
-    u_queue.push(U)
+    u_queue.push(&U);
   }
   
   // Apply the calculated voltage U to the motor
